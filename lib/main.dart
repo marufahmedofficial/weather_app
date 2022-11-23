5aa658901efe7efb66040c1285716226325a25e2
Maruf Ahmed <mk6116055@gmail.com>
2022-11-23 19:03:16 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:weather_app/pages/home_page.dart';
+import 'package:weather_app/pages/settings_page.dart';
 import 'package:weather_app/provider/weather_provider.dart';
 
 void main() {
- runApp(MultiProvider(
-     providers: [
-       ChangeNotifierProvider(create: (context)=> WeatherProvider() )
-     ],
-     child: const MyApp()));
+  runApp(MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context)=> WeatherProvider() )
+      ],
+      child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -21,14 +23,14 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Weather App',
       theme: ThemeData(
-
+        textTheme: GoogleFonts.merriweatherSansTextTheme(),
         primarySwatch: Colors.blue,
       ),
-     initialRoute: HomePage.routeName,
+      initialRoute: HomePage.routeName,
       routes: {
-      HomePage.routeName:(context) => HomePage()
+        HomePage.routeName:(context) => HomePage(),
+        SettingsPage.routeName: (context) => const SettingsPage(),
       },
     );
   }
 }
-
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
-
 import '../provider/weather_provider.dart';
+import '../utils/constants.dart';
+import '../utils/helper_functions.dart';
+import '../utils/weather_preferences.dart';
+import 'settings_page.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = '/';
+
   const HomePage({Key? key}) : super(key: key);
 
   @override
@@ -15,40 +19,70 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   late WeatherProvider weatherProvider;
   bool calledOnce = true;
-  bool isLoading = true;
+
   @override
   void didChangeDependencies() {
     if (calledOnce) {
       weatherProvider = Provider.of<WeatherProvider>(context);
-      _determinePosition().then((position) {
-        print('Latitude : ${position.latitude} and ${position.longitude}');
-        weatherProvider.getCurrentWeatherData(position).then((value) {
-          setState(() {
-            isLoading = false;
-          });
-        });
-      });
+      _getData();
     }
     calledOnce = false;
     super.didChangeDependencies();
   }
 
+  void _getData() async {
+    final position = await _determinePosition();
+    weatherProvider.setNewLocation(position.latitude, position.longitude);
+    final tempUnitStatus = await getBool(tempUnitKey);
+    final timeFormatStatus = await getBool(timeFormatKey);
+    weatherProvider.setTimePattern(timeFormatStatus);
+    weatherProvider.setTempUnit(tempUnitStatus);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       appBar: AppBar(
-        title: Text('Weather App'),
-      ),
-      body: ListView(
-        children: [
-          isLoading
-              ? CircularProgressIndicator()
-              : Text(
-            '${weatherProvider.currentWeatherResponse.main!.temp}',
-            style: TextStyle(fontSize: 24),
-          )
+        elevation: 0,
+        title: const Text('Weather App'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.my_location),
+          ),
+          IconButton(
+            onPressed: () {
+              showSearch(
+                context: context,
+                delegate: _CitySearchDelegate(),
+              ).then((city) {
+                if(city != null && city.isNotEmpty) {
+                  weatherProvider.convertAddressToLatLng(city);
+                }
+              });
+            },
+            icon: const Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () =>
+                Navigator.pushNamed(context, SettingsPage.routeName),
+            icon: const Icon(Icons.settings),
+          ),
         ],
       ),
+      body: weatherProvider.hasDataLoaded
+          ? ListView(
+              children: [
+                _currentWeatherSection(),
+                _forecastWeatherSection(),
+              ],
+            )
+          : const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ),
     );
   }
 
@@ -88,4 +122,186 @@ class _HomePageState extends State<HomePage> {
     // continue accessing the position of the device.
     return await Geolocator.getCurrentPosition();
   }
+
+  Widget _currentWeatherSection() {
+    final current = weatherProvider.currentWeatherResponse;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Text(
+            getFormattedDate(current!.dt!, pattern: 'MMM dd yyyy'),
+            style: txtNormal16White54,
+          ),
+          Text(
+            '${current.name}, ${current.sys!.country}',
+            style: txtAddressWhite24,
+          ),
+          Text(
+            '${current.main!.temp!.round()}$degree${weatherProvider.tempUnitSymbol}',
+            style: txtTempBig80,
+          ),
+          Text(
+            'Feels like ${current.main!.feelsLike!.round()}$degree${weatherProvider.tempUnitSymbol}',
+            style: txtTempSmall18,
+          ),
+          Image.network('$iconPrefix${current.weather![0].icon}$iconSuffix'),
+          Text(
+            current.weather![0].description!,
+            style: txtNormalWhite16,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Wrap(
+              children: [
+                Text(
+                  'Humidity ${current.main!.humidity}%  ',
+                  style: txtNormal16White54,
+                ),
+                Text(
+                  'Pressure ${current.main!.pressure} hPa  ',
+                  style: txtNormal16White54,
+                ),
+                Text(
+                  'Wind ${current.wind!.speed} m/s  ',
+                  style: txtNormal16White54,
+                ),
+                Text(
+                  'Wind Degree ${current.wind!.deg}$degree  ',
+                  style: txtNormal16White54,
+                ),
+                Text(
+                  'Visibility ${current.visibility} meter  ',
+                  style: txtNormal16White54,
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Wrap(
+              children: [
+                Text(
+                  'Sunrise ${getFormattedDate(current.sys!.sunrise!, pattern: weatherProvider.timePattern)}  ',
+                  style: txtNormalWhite16,
+                ),
+                Text(
+                  'Sunset ${getFormattedDate(current.sys!.sunset!, pattern: weatherProvider.timePattern)}  ',
+                  style: txtNormalWhite16,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _forecastWeatherSection() {
+    final forecastList = weatherProvider.forecastWeatherResponse!.list!;
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        shrinkWrap: true,
+        scrollDirection: Axis.horizontal,
+        itemCount: forecastList.length,
+        itemBuilder: (context, index) {
+          final item = forecastList[index];
+          return Container(
+            alignment: Alignment.center,
+            margin: const EdgeInsets.all(8),
+            padding: const EdgeInsets.all(8),
+            width: 130,
+            height: 200,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: Colors.black26,
+            ),
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    getFormattedDate(item.dt!,
+                        pattern: 'EEE ${weatherProvider.timePattern}'),
+                    style: txtNormalWhite16,
+                  ),
+                  Image.network(
+                    '$iconPrefix${item.weather![0].icon}$iconSuffix',
+                    width: 40,
+                    height: 40,
+                  ),
+                  Text(
+                    '${item.main!.tempMax!.round()}/${item.main!.tempMin!.round()}$degree${weatherProvider.tempUnitSymbol}',
+                    style: txtNormalWhite16,
+                  ),
+                  Text(
+                    item.weather![0].description!,
+                    style: txtNormalWhite16,
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
 }
+
+class _CitySearchDelegate extends SearchDelegate<String> {
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        onPressed: () {
+          query = '';
+        },
+        icon: const Icon(Icons.clear),
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      onPressed: () {
+        close(context, '');
+      },
+      icon: const Icon(Icons.arrow_back),
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return ListTile(
+      onTap: () {
+        close(context, query);;
+      },
+      title: Text(query),
+      leading: const Icon(Icons.search),
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    final filteredList = query.isEmpty ? cities :
+        cities.where((city) => city.toLowerCase().startsWith(query.toLowerCase())).toList();
+    return ListView.builder(
+      itemCount: filteredList.length,
+      itemBuilder: (context, index) {
+        final item = filteredList[index];
+        return ListTile(
+          onTap: () {
+            query = item;
+            close(context, query);
+          },
+          title: Text(item),
+        );
+      },
+    );
+  }
+
+}
+
+
