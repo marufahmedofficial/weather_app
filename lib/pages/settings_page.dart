6ed7cc83741d2108a21c46d1c3d5434272f788ba
Maruf Ahmed <mk6116055@gmail.com>
2023-01-17 22:51:45 +0600

    updated

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import '../provider/weather_provider.dart';
 import '../utils/weather_preferences.dart';
 
+
 class SettingsPage extends StatefulWidget {
   static const String routeName = '/settings';
   const SettingsPage({Key? key}) : super(key: key);
